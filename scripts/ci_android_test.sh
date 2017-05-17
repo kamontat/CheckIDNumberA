0e5c166d3128df42211afb908e4d499b14cb6d65
kamontat <kamontat_c@hotmail.com>
2017-05-17 20:45:23 +0700

    update android sdk tools

--- scripts/ci_android_test.sh
+++ scripts/ci_android_test.sh
@@ -2,17 +2,17 @@
 
 export ANDROID_EMULATOR_FORCE_32BIT='true'
 
-# list abi
-android update sdk --no-ui --all --filter 1
+# update android
+echo y | $ANDROID_HOME/tools/bin/sdkmanager --update
 
 # create emulation
 android create avd --name "Pixel_25" --device "pixel" --target 2 --abi "google_apis/x86"
 
-# run emulator
-emulator @Pixel_25
-
 # android tool 25.3.0 or later
 # $ANDROID_HOME/tools/bin/avdmanager create avd --name "Pixel_25" -d "pixel" -k "system-images;android-25;google_apis;x86"
+
+# run emulator
+emulator @Pixel_25
 
 # run android test
 # ./gradlew --no-daemon --stacktrace :app:connectedAndroidTest -PDisableRibbon
