d2ab66aaeb4f6c87db2551b3dd93ba10da55e454
kamontat <kamontat_c@hotmail.com>
2017-05-17 19:52:35 +0700

    create avd (Fixed)

--- scripts/ci_android_test.sh
+++ scripts/ci_android_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # run emulation
-android create avd --name "pixel_25" --path ./.android --device "pixel"
+android create avd --name "Pixel_25" --path ./.android --device "pixel" -k "system-images;android-25;google_apis;x86"
 
 
 # create pixel device
