2e1550458f1009cc67e4532bc7c5baf075c06224
kamontat <kamontat_c@hotmail.com>
2017-05-17 19:50:41 +0700

    try to createavd

--- scripts/ci_android_test.sh
+++ scripts/ci_android_test.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
 # run emulation
-ls $ANDROID_HOME/tools/
+android create avd --name "pixel_25" --path ./.android --device "pixel"
 
-# $ANDROID_HOME/tools/bin/avdmanager -h
-# $ANDROID_HOME/tools/bin/avdmanager list
-# $ANDROID_HOME/tools/bin/avdmanager list avd
-# $ANDROID_HOME/tools/bin/avdmanager list target
-# $ANDROID_HOME/tools/bin/avdmanager list device
 
 # create pixel device
 # $ANDROID_HOME/tools/bin/avdmanager create avd --name "Pixel_25" -d "pixel" -k "system-images;android-25;google_apis;x86"
