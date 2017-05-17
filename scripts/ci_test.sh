060b0e9699db88a7884cf9bac91275cae4f177d0
kamontat <kamontat_c@hotmail.com>
2017-05-17 18:26:42 +0700

    add android test

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# run unit test
 ./gradlew --no-daemon --stacktrace testDebugUnitTest :app:testDebugUnitTest -PDisableRibbon
+# run android test
+./gradlew --no-daemon --stacktrace :app:connectedAndroidTest -PDisableRibbon
