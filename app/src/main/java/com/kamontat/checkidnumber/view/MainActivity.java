1942e9226e2ef9e3192972ae0a1c483e3efe3ba4
kamontat <kamontat_c@hotmail.com>
2017-05-18 00:16:46 +0700

    Add many method to main activity

--- app/src/main/java/com/kamontat/checkidnumber/view/MainActivity.java
+++ app/src/main/java/com/kamontat/checkidnumber/view/MainActivity.java
@@ -1,14 +1,21 @@
 package com.kamontat.checkidnumber.view;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import com.kamontat.checkidnumber.R;
 import com.kamontat.checkidnumber.adapter.ViewPagerAdapter;
+import com.kamontat.checkidnumber.api.constants.Status;
+import com.kamontat.checkidnumber.model.IDNumber;
 import com.kamontat.checkidnumber.model.Pool;
 import com.kamontat.checkidnumber.presenter.MainPresenter;
 import com.kamontat.checkidnumber.view.fragment.InputFragment;
@@ -31,12 +38,12 @@ public class MainActivity extends AppCompatActivity implements MainView {
 				case R.id.navigation_insert:
 					viewPager.setCurrentItem(0);
 					item.setChecked(true);
-					// Log.d("NEXT PAGE", "Input Page");
+					showKeyBoard();
 					return true;
 				case R.id.navigation_show:
 					viewPager.setCurrentItem(1);
 					item.setChecked(true);
-					// Log.d("NEXT PAGE", "List Page");
+					hideKeyBoard();
 					return true;
 			}
 			return false;
@@ -53,6 +60,8 @@ public class MainActivity extends AppCompatActivity implements MainView {
 			for (int i = 0; i < navigation.getMenu().size(); i++)
 				navigation.getMenu().getItem(i).setChecked(false);
 			navigation.getMenu().getItem(position).setChecked(true);
+			if (navigation.getMenu().getItem(position).getItemId() == R.id.navigation_insert) showKeyBoard();
+			else if (navigation.getMenu().getItem(position).getItemId() == R.id.navigation_show) hideKeyBoard();
 		}
 		
 		@Override
@@ -78,6 +87,9 @@ public class MainActivity extends AppCompatActivity implements MainView {
 	
 	private void setViewPaperAdapter() {
 		inputFragment = new InputFragment();
+		inputFragment.setButton(getInputOnClick());
+		inputFragment.setInputListener(getInputTextChange());
+		
 		listFragment = new ListFragment();
 		listFragment.setListAdapter(presenter.getPool()); // mockup
 		
@@ -85,5 +97,69 @@ public class MainActivity extends AppCompatActivity implements MainView {
 		adapter.addFragment(inputFragment);
 		adapter.addFragment(listFragment);
 		viewPager.setAdapter(adapter);
+	}
+	
+	private TextWatcher getInputTextChange() {
+		return new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+			}
+			
+			@Override
+			public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+			}
+			
+			@Override
+			public void afterTextChanged(Editable editable) {
+				updateInput();
+			}
+		};
+	}
+	
+	private View.OnClickListener getInputOnClick() {
+		return new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				checkAndInsert();
+			}
+		};
+	}
+	
+	private void updateInput() {
+		updateInput(new IDNumber(getInputText()));
+	}
+	
+	@Override
+	public String getInputText() {
+		return inputFragment.getInput();
+	}
+	
+	@Override
+	public void checkAndInsert() {
+		IDNumber id = new IDNumber(getInputText());
+		if (id.getStatus() == Status.OK) {
+			presenter.addID(id);
+			inputFragment.clearText();
+		} else updateInput(id);
+	}
+	
+	@Override
+	public void updateInput(IDNumber id) {
+		inputFragment.setTextColor(id.getStatus().getColor(getResources()));
+		inputFragment.setButtonEnable(id.getStatus() == Status.OK);
+	}
+	
+	@Override
+	public void hideKeyBoard() {
+		View v = getWindow().getCurrentFocus();
+		if (v != null) {
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+		}
+	}
+	
+	@Override
+	public void showKeyBoard() {
+		inputFragment.showKeyboard(this);
 	}
 }
--- app/src/main/java/com/kamontat/checkidnumber/view/MainView.java
+++ app/src/main/java/com/kamontat/checkidnumber/view/MainView.java
@@ -1,9 +1,20 @@
 package com.kamontat.checkidnumber.view;
 
+import com.kamontat.checkidnumber.model.IDNumber;
+
 /**
  * @author kamontat
  * @version 1.0
  * @since Mon 15/May/2017 - 1:23 AM
  */
 public interface MainView {
+	String getInputText();
+	
+	void checkAndInsert();
+	
+	void updateInput(IDNumber id);
+	
+	void hideKeyBoard();
+	
+	void showKeyBoard();
 }
