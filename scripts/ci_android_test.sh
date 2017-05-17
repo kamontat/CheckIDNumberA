ce6c6f56514e96f374e4cf7833a68056da0a672f
kamontat <kamontat_c@hotmail.com>
2017-05-17 21:03:20 +0700

    fix (2)

--- scripts/ci_android_test.sh
+++ scripts/ci_android_test.sh
@@ -4,12 +4,13 @@ export ANDROID_EMULATOR_FORCE_32BIT='true'
 
 # update android
 echo y | $ANDROID_HOME/tools/bin/sdkmanager --update
+echo y | $ANDROID_HOME/tools/bin/sdkmanager --verbose "system-images;android-25;google_apis;x86"
 
 # list
 $ANDROID_HOME/tools/bin/avdmanager list
 
 # android tool 25.3.0 or later
-$ANDROID_HOME/tools/bin/avdmanager create avd --name "Pixel_25" -d "pixel" -k "system-images;android-25;google_apis;x86"
+$ANDROID_HOME/tools/bin/avdmanager create avd -n test -d pixel -k system-images;android-25;google_apis;x86 -c 1000M
 
 # run emulator
 emulator @Pixel_25
