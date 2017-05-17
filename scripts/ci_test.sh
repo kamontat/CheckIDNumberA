0c797e4acbe859e164d3363d2af521a736755878
kamontat <kamontat_c@hotmail.com>
2017-05-17 18:41:04 +0700

    testing (1)

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -3,4 +3,5 @@
 # run unit test
 ./gradlew --no-daemon --stacktrace testDebugUnitTest :app:testDebugUnitTest -PDisableRibbon
 # run android test
+start_emulator.sh -h
 ./gradlew --no-daemon --stacktrace :app:connectedAndroidTest -PDisableRibbon
