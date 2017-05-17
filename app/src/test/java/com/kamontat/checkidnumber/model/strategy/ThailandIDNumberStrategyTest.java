4de99e8ba157895fec739c731f84593a660bb662
kamontat <kamontat_c@hotmail.com>
2017-05-17 23:15:45 +0700

    Add new test and fixed previous test

---
+++ app/src/test/java/com/kamontat/checkidnumber/model/strategy/ThailandIDNumberStrategyTest.java
@@ -0,0 +1,46 @@
+package com.kamontat.checkidnumber.model.strategy;
+
+import com.kamontat.checkidnumber.api.constants.Status;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since Wed 17/May/2017 - 11:10 PM
+ */
+public class ThailandIDNumberStrategyTest {
+	IDNumberStrategy strategy;
+	
+	@Before
+	public void setUp() throws Exception {
+		strategy = new ThailandIDNumberStrategy();
+	}
+	
+	@Test
+	public void getLengthOfThaiID() throws Exception {
+		Assert.assertEquals(13, strategy.getIDLength());
+	}
+	
+	@Test
+	public void shouldOK() throws Exception {
+		Status s = strategy.checking("110060036196");
+		Assert.assertEquals(s, Status.OK);
+	}
+	
+	@Test
+	public void shouldNotCreate() throws Exception {
+		Status s = strategy.checking("");
+		Assert.assertEquals(s, Status.NOT_CREATE);
+		
+		Status ss = strategy.checking(null);
+		Assert.assertEquals(s, Status.NOT_CREATE);
+	}
+	
+	@Test
+	public void shouldNot_create() throws Exception {
+		Status s = strategy.checking("4839586954321");
+		Assert.assertEquals(s, Status.UNCORRECTED);
+	}
+}
