a92fa9120dd554d4bfbd48ede4258bb4a6e89a43
kamontat <kamontat_c@hotmail.com>
2017-05-19 19:42:43 +0700

    Wrong test (Fixed)

--- app/src/test/java/com/kamontat/checkidnumber/MainJunitTest.java
+++ app/src/test/java/com/kamontat/checkidnumber/MainJunitTest.java
@@ -1,6 +1,8 @@
 package com.kamontat.checkidnumber;
 
 import com.kamontat.checkidnumber.model.IDNumberTest;
+import com.kamontat.checkidnumber.model.ThaiIDNumberTest;
+import com.kamontat.checkidnumber.model.strategy.ThailandIDNumberStrategyTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -10,6 +12,6 @@ import org.junit.runners.Suite;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses(IDNumberTest.class)
+@Suite.SuiteClasses({IDNumberTest.class, ThaiIDNumberTest.class, ThailandIDNumberStrategyTest.class})
 public class MainJunitTest {
 }
