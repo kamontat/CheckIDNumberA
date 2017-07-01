f45aa9ce82e4bc61b3fbcd18bd6835d0bd09c42a
kamontat <kamontat_c@hotmail.com>
2017-07-01 19:35:54 +0700

    update scrite

--- scripts/ci_test.sh
+++ scripts/ci_test.sh
@@ -3,4 +3,4 @@
 # run unit test
 echo "--------------------------- Starting Android Test with Coverage ---------------------------"
 
-./gradlew createDebugCoverageReport --profile -PdisablePreDex
+./gradlew createDebugCoverageReport --profile
